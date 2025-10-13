6232b945f98c72ec56b977b52461d264e5e4c310
Nyxiie-Nek <ribeiro.cruz.victor@escola.pr.gov.br>
2025-10-13 09:19:44 -0300

    3-1

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,25 @@
 criaCartao(
-    'Categoria',
-    'Pergunta',    
-    'Resposta'
+    '3-1',
+    'Witch video encryptor should I use?',    
+    'Any video encryptor should do the trick'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',    
-    'Resposta'
+    '3-1',
+    'Anyone?',    
+    'No, not anyone'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',    
-    'Resposta'
+    '3-1',
+    'You said Any video encryptor should do the trick',    
+    "Yes, that's correct"
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',    
-    'Resposta'
+    '3-1',
+    'So, anyone?',    
+    'No, not anyone'
 )
 
 criaCartao(
