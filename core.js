be3ac5f8a3d924c53ef5f0d932a5b0a78f6f09fa
Nyxiie-Nek <ribeiro.cruz.victor@escola.pr.gov.br>
2025-10-16 07:31:42 -0300

    a

--- core.js
+++ core.js
@@ -4,3 +4,13 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao';
 }
 
+cartao.innerHTML = 
+                <div class="cartao_conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>O que é JavaScript?</p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>O JavaScript é uma liguagem de programação</p>
+                    </div>
+                </div>
